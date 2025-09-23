39fbcd7e71ec2065b37e0e8c4c40cf0b489ff0c4
Matheus Pedroso <matheus_pedroso2012@hotmail.com>
2025-09-22 21:45:00 -0300

    :construction: stating to implement service of Authentication

---
+++ Mango.Services.AuthAPI/Program.cs
@@ -0,0 +1,63 @@
+
+using Mango.Services.AuthAPI.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.AuthAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+               options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // For Identity
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+            
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+            ApplyMigration();
+            app.Run();
+
+
+            // Apply pending Migration whenever application restarts 
+            void ApplyMigration()
+            {
+                using (var scope = app.Services.CreateScope())
+                {
+                    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                    if (dbContext.Database.GetPendingMigrations().Count() > 0)
+                    {
+                        dbContext.Database.Migrate();
+                    }
+                }
+            }
+        }
+    }
+}
