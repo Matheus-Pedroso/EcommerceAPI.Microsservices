659c42094b5b87fdba24c3c1fd10ebba919cd4f5
Matheus Pedroso <matheus_pedroso2012@hotmail.com>
2025-10-24 23:56:38 -0300

    :sparkles: implementing dataTable framework to the OrderController

---
+++ Mango.Web/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers;
+
+public class OrderController(IOrderService orderService) : Controller
+{
+    public IActionResult OrderIndex()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        IEnumerable<OrderHeaderDTO> list;
+        string userId = string.Empty;
+        if (!User.IsInRole(StaticDetails.RoleAdmin))
+        {
+            userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault().Value;
+        }
+        ResponseDTO responseDTO = orderService.GetAllOrder(userId).GetAwaiter().GetResult();
+        if (responseDTO != null)
+        {
+            list = JsonConvert.DeserializeObject<List<OrderHeaderDTO>>(Convert.ToString(responseDTO.Result));
+        }
+        else
+        {
+            list = new List<OrderHeaderDTO>();
+        }
+        return Json(new { data = list });
+    }
+}
---
+++ Mango.Web/wwwroot/js/order.js
@@ -0,0 +1,14 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = new DataTable('#tblData',{
+        "ajax": "/order/getall",
+        "columns": [
+            { data: 'orderheaderid', className: 'w-10' },
+        ]
+    })
+}
