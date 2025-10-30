cbf945b07ef850e234c4687a1b5f9fd01dc4ee3d
Matheus Pedroso <matheus_pedroso2012@hotmail.com>
2025-10-29 21:27:48 -0300

    :sparkles: implemeting ocelot gateway

---
+++ Mango.GatewaySolution/Program.cs
@@ -0,0 +1,23 @@
+using Mango.GatewaySolution.Extensions;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Mango.GatewaySolution;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddOcelot();
+        builder.AddAppAuthentication();
+
+
+        var app = builder.Build();
+        
+
+        app.MapGet("/", () => "Hello World!");
+        app.UseOcelot();
+        app.Run();
+    }
+}
